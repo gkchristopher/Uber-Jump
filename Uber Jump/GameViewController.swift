9cb34b970ea2609e99346e8ff5550432a6fc8644
Christopher Moore <gkchristopher@icloud.com>
2019-01-19 06:33:34 -0500

    Initial Commit

---
+++ Uber Jump/GameViewController.swift
@@ -0,0 +1,39 @@
+//Copyright © 2019 Roving Mobile. All rights reserved.
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let view = self.view as? SKView else { fatalError() }
+
+        let scene = GameScene(size: view.bounds.size)
+        scene.scaleMode = .aspectFit
+        view.presentScene(scene)
+        
+        view.ignoresSiblingOrder = true
+        
+        view.showsFPS = true
+        view.showsNodeCount = true
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
