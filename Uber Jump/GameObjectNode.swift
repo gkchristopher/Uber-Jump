5d9f5b3624e31a3553dfcbe8730e9f7a86a224cd
Christopher Moore <gkchristopher@icloud.com>
2019-01-19 11:16:58 -0500

    finish part one

---
+++ Uber Jump/GameObjectNode.swift
@@ -0,0 +1,62 @@
+//Copyright © 2019 Roving Mobile. All rights reserved.
+
+import SpriteKit
+
+enum StarType: Int {
+    case normal
+    case special
+}
+
+enum PlatformType: Int {
+    case normal
+    case `break`
+}
+
+class GameObjectNode: SKNode {
+
+    func collision(with player: SKNode) -> Bool {
+        return false
+    }
+
+    func checkNodeRemoval(playerY: CGFloat) {
+        if playerY < position.y + 300.0 {
+            removeFromParent()
+        }
+    }
+}
+
+class StarNode: GameObjectNode {
+
+    var starType: StarType!
+    let starSound = SKAction.playSoundFileNamed("StarPing.caf", waitForCompletion: false)
+
+    override func collision(with player: SKNode) -> Bool {
+        player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 400.0)
+        run(starSound) {
+            self.removeFromParent()
+        }
+        return true
+    }
+}
+
+class PlatformNode: GameObjectNode {
+
+    var platformType: PlatformType!
+
+    override func collision(with player: SKNode) -> Bool {
+        if let dy = player.physicsBody?.velocity.dy, dy < CGFloat(0.0) {
+            player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 250.0)
+
+            if platformType == .break {
+                removeFromParent()
+            }
+        }
+        return false
+    }
+}
+
+struct CollisionCategoryBitmask {
+    static let Player: UInt32 = 0x00
+    static let Star: UInt32 = 0x01
+    static let Platform: UInt32 = 0x02
+}
--- Uber Jump/GameScene.swift
+++ Uber Jump/GameScene.swift
@@ -4,80 +4,168 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+    var player: SKNode!
+    var backgroundNode: SKNode!
+    var midgroundNode: SKNode!
+    var foregroundNode: SKNode!
+    var hudNode: SKNode!
+    var tapToStartNode: SKSpriteNode!
+    var scaleFactor: CGFloat!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    override init(size: CGSize) {
+        super.init(size: size)
+        backgroundColor = SKColor.white
+
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        physicsWorld.contactDelegate = self
+
+        scaleFactor = size.width / 320.0
+
+        backgroundNode = createBackgroundNode()
+        addChild(backgroundNode)
+
+        foregroundNode = createForegroundNode()
+        addChild(foregroundNode)
+
+        hudNode = createHUDNode()
+        addChild(hudNode)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+
+    func createBackgroundNode() -> SKNode {
+        let backgroundNode = SKNode()
+        let ySpacing = 64.0 * scaleFactor
+
+        for index in 0...19 {
+            let node = SKSpriteNode(imageNamed: String(format: "Background%02d", index + 1))
+            node.setScale(scaleFactor)
+            node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+            node.position = CGPoint(x: size.width / 2, y: ySpacing * CGFloat(index))
+
+            backgroundNode.addChild(node)
         }
+
+        return backgroundNode
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+
+    func createForegroundNode() -> SKNode {
+        let node = SKNode()
+
+        let platform = createPlatform(at: CGPoint(x: 160, y: 320), type: .normal)
+        node.addChild(platform)
+
+        let star = createStar(at: CGPoint(x: 160, y: 220), type: .special)
+        node.addChild(star)
+
+        player = createPlayer()
+        node.addChild(player)
+
+        return node
+    }
+
+    func createHUDNode() -> SKNode {
+        let node = SKNode()
+
+        tapToStartNode = SKSpriteNode(imageNamed: "TapToStart")
+        tapToStartNode.position = CGPoint(x: size.width / 2, y: 180.0)
+        node.addChild(tapToStartNode)
+
+        return node
+    }
+
+    func createPlayer() -> SKNode {
+        let playerNode = SKNode()
+        playerNode.position = CGPoint(x: size.width / 2, y: 80.0)
+
+        let sprite = SKSpriteNode(imageNamed: "Player")
+        playerNode.addChild(sprite)
+
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        playerNode.physicsBody?.isDynamic = false
+        playerNode.physicsBody?.allowsRotation = true
+        playerNode.physicsBody?.restitution = 1.0
+        playerNode.physicsBody?.friction = 0.0
+        playerNode.physicsBody?.angularDamping = 0.0
+        playerNode.physicsBody?.linearDamping = 0.0
+        playerNode.physicsBody?.usesPreciseCollisionDetection = true
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Player
+        playerNode.physicsBody?.collisionBitMask = 0
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryBitmask.Star | CollisionCategoryBitmask.Platform
+
+        return playerNode
+    }
+
+    func createStar(at position: CGPoint, type: StarType) -> StarNode {
+        let node = StarNode()
+        let nodePosition = CGPoint(x: position.x * scaleFactor, y: position.y)
+        node.position = nodePosition
+        node.name = "NODE_STAR"
+
+        var sprite: SKSpriteNode
+        switch type {
+        case .normal:
+            sprite = SKSpriteNode(imageNamed: "Star")
+        case .special:
+            sprite = SKSpriteNode(imageNamed: "StarSpecial")
         }
+        node.addChild(sprite)
+
+        node.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        node.physicsBody?.isDynamic = false
+        node.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Star
+        node.physicsBody?.collisionBitMask = 0
+
+        return node
     }
-    
+
+    func createPlatform(at position: CGPoint, type: PlatformType) -> PlatformNode {
+        let node = PlatformNode()
+        let nodePosition = CGPoint(x: position.x * scaleFactor, y: position.y)
+        node.position = nodePosition
+        node.name = "NODE_PLATFORM"
+        node.platformType = type
+
+        var sprite: SKSpriteNode
+        switch type {
+        case .normal:
+            sprite = SKSpriteNode(imageNamed: "Platform")
+        case .break:
+            sprite = SKSpriteNode(imageNamed: "PlatformBreak")
+        }
+        node.addChild(sprite)
+
+        node.physicsBody = SKPhysicsBody(rectangleOf: sprite.size)
+        node.physicsBody?.isDynamic = false
+        node.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Platform
+        node.physicsBody?.collisionBitMask = 0
+
+        return node
+    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard player.physicsBody?.isDynamic == false else { return }
+        tapToStartNode.removeFromParent()
+
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20.0))
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        var updateHUD = false
+        let whichNode = (contact.bodyA.node != player) ? contact.bodyA.node : contact.bodyB.node
+        let other = whichNode as! GameObjectNode
+
+        updateHUD = other.collision(with: player)
+
+        if updateHUD {
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
